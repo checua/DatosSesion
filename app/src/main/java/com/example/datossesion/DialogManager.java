d53e764ccc06db035d68fcc996ba3ac2d782f83d
Che <profesor76@hotmail.com>
2022-06-03 14:18:40 -0500

    Uno

--- app/src/main/java/com/example/datossesion/DialogManager.java
+++ app/src/main/java/com/example/datossesion/DialogManager.java
@@ -1,9 +1,12 @@
 package com.example.datossesion;
 
+import android.app.AlertDialog;
 import android.content.Context;
 
 public class DialogManager {
 
-    public void showAlertDialog(Context context, String title, String message, Boolean status)
+    public void showAlertDialog(Context context, String title, String message, Boolean status){
+        AlertDialog alertDialog = new AlertDialog.Builder ( context ).create ();
+    }
 
 }
