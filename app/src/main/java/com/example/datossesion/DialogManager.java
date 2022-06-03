061e009f139e4993430d7c5d4fdad8b402d84c5f
Che <profesor76@hotmail.com>
2022-06-03 14:17:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/datossesion/DialogManager.java
@@ -0,0 +1,9 @@
+package com.example.datossesion;
+
+import android.content.Context;
+
+public class DialogManager {
+
+    public void showAlertDialog(Context context, String title, String message, Boolean status)
+
+}
