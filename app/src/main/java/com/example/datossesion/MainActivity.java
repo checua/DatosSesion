8903307ed150f635f75d1e1f032f68b82394a4b3
Che <profesor76@hotmail.com>
2022-06-22 14:30:14 -0500

    5 Internet permission ok 6 Porgressbar ok 7 Resgites session ok 8 Log and REgister ok 9 Menu ok

--- app/src/main/java/com/example/datossesion/MainActivity.java
+++ app/src/main/java/com/example/datossesion/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.datossesion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -7,6 +8,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -50,6 +54,31 @@ public class MainActivity extends AppCompatActivity {
             session.logOut();
             //finish ();
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater ();
+        inflater.inflate ( R.menu.menu, menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId ()){
+            case R.id.ajustes:
+                Toast.makeText ( getApplicationContext (), "Has pulsado Ajustes", Toast.LENGTH_LONG ).show ();
+                return true;
+            case R.id.ayuda:
+                Toast.makeText ( getApplicationContext (), "Has pulsado Ayuda", Toast.LENGTH_LONG ).show ();
+                return true;
+            case R.id.extra:
+                Toast.makeText ( getApplicationContext (), "Has pulsado Extra", Toast.LENGTH_LONG ).show ();
+                return true;
+            default:
+                return super.onOptionsItemSelected ( item );
+        }
+
     }
 /*
     @Override
